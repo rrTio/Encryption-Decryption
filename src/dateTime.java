6b6a230d8b45c781ba9e5be08cc625ba06992937
rrTio <tio.rozsraven@ue.edu.ph>
2024-04-17 23:45:52 +0800

    Added date and time

---
+++ src/dateTime.java
@@ -0,0 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+public class dateTime {
+    public static void main(String args[]) throws InterruptedException {
+
+        while (true){
+            DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+            DateTimeFormatter time = DateTimeFormatter.ofPattern("HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            String currentTime = time.format(now);
+            String currentDate = date.format(now);
+            System.out.println(currentDate);
+            System.out.println(currentTime);
+            TimeUnit.SECONDS.sleep(1);
+        }
+
+    }
+
+}
